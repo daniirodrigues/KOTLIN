8f5bfde32cf43508760ec098b3dd454c043b3686
daniel.rsilva73 <optydanierodrigues9092@gmail.com>
2026-03-12 10:22:04 -0300

    HORA DE CODAR KOTLIN

---
+++ 1.kt
@@ -0,0 +1,8 @@
+fun main() {
+// Escreva um programa que crie uma variável chamada "nome_do_carro" e atribua-a
+// um valor "Fusca". Exiba-a ao usuário.
+
+    val nomeDocarro = "Fusca"
+    println(nomeDocarro)
+
+}
