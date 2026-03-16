015b9d7efa5365042966a0b5bcebbfa468eb0338
Daniel Rodrigues da Silva <odani7544@gmail.com>
2026-03-15 22:27:51 -0300

    Add files via upload

---
+++ 8.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("--- CÁLCULO DA ÁREA DO PARALELOGRAMO ---")
+
+
+    print("Digite o valor da base: ")
+    val base = readln().toDouble()
+
+    print("Digite o valor da altura: ")
+    val altura = readln().toDouble()
+
+
+    val area = base * altura
+
+    println("O valor da área do paralelogramo é: $area")
+}
