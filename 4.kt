5e973ac53b22075b7a5961972da0e34a0c274748
Daniel Rodrigues da Silva <odani7544@gmail.com>
2026-03-15 22:26:42 -0300

    Add files via upload

---
+++ 4.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+ println("--- CÁLCULO DA ÁREA DO RETANGULO ---")
+
+
+println("Digite a Base:")
+ val base = readln().toDouble()
+
+println("Digite a Altura:")
+    val altura = readln().toDouble()
+
+
+    val area = base * altura
+    println ("O valor do cálculo da área do retangulo é: $area")
+
+}
