542cb2d5ad3aef5014f39e185b13ea090d45cc39
Daniel Rodrigues da Silva <odani7544@gmail.com>
2026-03-15 22:26:55 -0300

    Add files via upload

---
+++ 5.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("--- CÁLCULO DA ÁREA DO QUADRADO---")
+
+
+    println("Digite o lado:")
+    val lado = readln().toDouble()
+
+    val area = lado * lado
+    println ("O valor do cálculo do quadrado é: $area")
+
+}
