823d5e924aa9d04f0d68cef02f998dd06c0c54f8
Daniel Rodrigues da Silva <odani7544@gmail.com>
2026-03-15 21:19:25 -0300

    Add files via upload

---
+++ 3.kt
@@ -0,0 +1,13 @@
+fun main () {
+// Escreva um programa em que o usuário informe o seu nome e em seguida o programa perguntará a idade do usuário. Agora o programa deve exibir a mensagem
+// "Olá, [NomeDoUsuario], sua idade é [idade]".
+
+    println ("Digite seu Nome: ")
+    val nome = readln()
+
+    println ("E agora digite sua idade: ")
+    val idade = readln()
+
+
+    println("Olá $nome, Sua Idade é $idade anos")
+}
