0ccba7083406ccac21bc1781022d4d673c45143f
Daniel Rodrigues da Silva <odani7544@gmail.com>
2026-03-16 00:05:10 -0300

    Add files via upload

---
+++ 18.kt
@@ -0,0 +1,19 @@
+fun main () {
+    // Escreva um programa que calcule a média de quatro números informados pelo usuário, mas somente se esses números forem maiores que 0 e menores que 10. No final, se a média for maior que cinco o usuário receberá uma mensagem "Você passou no teste". Em qualquer outra situação, ele receberá uma mensagem de "tente novamente"
+
+    println("Digite um primeiro número:")
+    val nm1 = readln().toDouble()
+
+    println("Digite um Segundo número: ")
+    val nm2 = readln().toDouble()
+
+    println("Digite um Terceiro número:")
+    val nm3 = readln().toDouble()
+
+    println("Digite um Quarto Número:")
+    val nm4 = readln().toDouble()
+
+    
+
+
+}
